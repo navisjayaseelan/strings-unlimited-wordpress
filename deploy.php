ec96004b78fc6971b9f2b19a43e87d4b977e2c65
Navis <michael.bearly@hotmail.com>
2019-02-15 02:07:12 +0530

    jungle

--- deploy.php
+++ deploy.php
@@ -1,12 +1,4 @@
 <?php
-	/**
-	 * GIT DEPLOYMENT SCRIPT
-	 *
-	 * Used for automatically deploying websites via github or bitbucket, more deets here:
-	 * https://gist.github.com/riodw/71f6e2244534deae652962b32b7454e2
-	 * How To Use:
-	 * https://medium.com/riow/deploy-to-production-server-with-git-using-php-ab69b13f78ad
-	 */
 	// The commands
 	$commands = array(
 		'echo $PWD',
