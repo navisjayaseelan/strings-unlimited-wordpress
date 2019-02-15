454fa91f6347a2f5be32de78f9d883feb554f6c6
Navis <michael.bearly@hotmail.com>
2019-02-16 00:02:28 +0530

    contact form

--- wp-content/themes/bootstrap2wordpress/page-contactus.php
+++ wp-content/themes/bootstrap2wordpress/page-contactus.php
@@ -4,35 +4,31 @@
 ?>
   <section class="content">
         <div class="container">
-            <div class="row">
-                <div class="col-lg-12 col-12 ">
-					<div class="row">
-						<div class="col">
-							<h2 align="center" class="text-primary">ORDER ENQUIRY FORM</h2>
-					       	<hr/>
-							<form action="/action_page.php">
-							<div class="form-group">
-								<label for="firstname">First Name</label>
-								<input type="text" class="form-control" id="firstname">
-							</div>
-							<div class="form-group">
-								<label for="company">Company:</label>
-								<input type="text" class="form-control" id="company">
-							</div>
-							<div class="form-group form-check">
-								<label class="form-check-label">
-								<input class="form-check-input" type="checkbox"> Remember me
-							</label>
-							</div>
-							<button type="submit" class="btn btn-primary">Submit</button>
-							</form>
-						</div>
-						<div class="col">
-							<h2 align="center" class="text-primary">OUR OFFICE</h2>
-						</div>
+			<div class="row">
+				<div class="col">
+					<h2 align="center" class="text-primary">ORDER ENQUIRY FORM</h2>
+			       	<hr/>
+					<form action="/action_page.php">
+					<div class="form-group">
+						<label for="firstname">First Name</label>
+						<input type="text" class="form-control" id="firstname">
 					</div>
+					<div class="form-group">
+						<label for="company">Company:</label>
+						<input type="text" class="form-control" id="company">
+					</div>
+					<div class="form-group form-check">
+						<label class="form-check-label">
+						<input class="form-check-input" type="checkbox"> Remember me
+					</label>
+					</div>
+					<button type="submit" class="btn btn-primary">Submit</button>
+					</form>
 				</div>
-            </div>
+				<div class="col">
+					<h2 align="center" class="text-primary">OUR OFFICE</h2>
+				</div>
+			</div>
         </div>
     </section>
 <?php
