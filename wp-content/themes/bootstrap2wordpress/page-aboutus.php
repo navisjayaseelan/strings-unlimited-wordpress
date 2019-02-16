c86e724a514eb865037571be11b17201ac282cc4
Navis <michael.bearly@hotmail.com>
2019-02-16 17:51:40 +0530

    Added pages and images

---
+++ wp-content/themes/bootstrap2wordpress/page-aboutus.php
@@ -0,0 +1,24 @@
+ <?php
+/* Template Name: About us us for Strings Unlimited */
+	get_header();
+?>
+ <!--------- about-us-content start ----->
+    <section class="about-us-content">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 col-12 about-us-content-sub">
+                    <p>
+                        Strings Unlimited is a Global Sourcing Company registered in United Kingdom (company number – SC 477164) and strategically operates from India through its subsidiary company Strings Factory Private Limited. The company manufactures and supplies Apparel, Textile and Home furnishing products through its in-house production unit and also through our wide network of factories. The company also does research and development and provides customization services.
+                    </p>
+                    <a href="why-us.html">
+                        <button>Why Us</button>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </section>
+<!--------- about-us-content end ------->
+<?php
+/* Template Name: Contact us for Strings Unlimited */
+	get_header();
+?>
---
+++ wp-content/themes/bootstrap2wordpress/page-whyus.php
@@ -0,0 +1,33 @@
+<?php
+/* Template Name: Why us for Strings Unlimited */
+	get_header();
+?>
+ <!--------- why-us-content start ----->
+    <section class="why-us-content">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 col-12 why-us-content-sub">
+                	<h2>
+                        Why us
+                    </h2>
+                    <p>
+                        <ul style="font-size: larger;">
+                            <li>Presence in UK and India ensures zero communication barrier</li>
+                            <li>A Team of trained professionals who bring with them rich experience in every aspect of the apparel industry</li>
+                            <li>We only work with reliable suppliers and we grade them on a regular basis within our ERP system</li>
+                            <li>Good working relationship with Suppliers/Factories in India and Bangladesh to ensure we provide the competitive edge</li>
+                            <li>We as a company are system driven with all activities recorded in our ERP software ensuring professional service with regular manual and automated updates during production</li>
+                        </ul>
+                    </p>
+                    <!-- <a href="why-us.html">
+                        <button>Why Us</button>
+                    </a> -->
+                </div>
+            </div>
+        </div>
+    </section>
+<!--------- why-us-content end ------->
+<?php
+/* Template Name: Contact us for Strings Unlimited */
+	get_header();
+?>
