bc5521e871e3f3710749e4e8c191482793b20540
Navis <michael.bearly@hotmail.com>
2019-02-16 18:46:17 +0530

    Dynamic Why US

--- wp-content/themes/bootstrap2wordpress/page-whyus.php
+++ wp-content/themes/bootstrap2wordpress/page-whyus.php
@@ -1,22 +1,19 @@
 <?php
 /* Template Name: Why us for Strings Unlimited */
-	get_header();
+get_header();
+#Advanced custom field added by Navis
+$title = get_field('title');
+$content = get_field('content')
 ?>
  <!--------- why-us-content start ----->
     <section class="why-us-content">
         <div class="container">
             <div class="row">
                 <div class="col-lg-12 col-12 why-us-content-sub">
-                	<h2 align="center" style="color: #00bfff">Why us</h2>
+                	<h2 align="center" style="color: #00bfff"><?php echo $title ?></h2>
                     <br/>
                     <p>
-                        <ul style="font-size: larger;">
-                            <li>Presence in UK and India ensures zero communication barrier</li>
-                            <li>A Team of trained professionals who bring with them rich experience in every aspect of the apparel industry</li>
-                            <li>We only work with reliable suppliers and we grade them on a regular basis within our ERP system</li>
-                            <li>Good working relationship with Suppliers/Factories in India and Bangladesh to ensure we provide the competitive edge</li>
-                            <li>We as a company are system driven with all activities recorded in our ERP software ensuring professional service with regular manual and automated updates during production</li>
-                        </ul>
+                        <?php echo $content ?>
                     </p>
                     <!-- <a href="why-us.html">
                         <button>Why Us</button>
@@ -28,5 +25,5 @@
 <!--------- why-us-content end ------->
 <?php
 /* Template Name: Contact us for Strings Unlimited */
-	get_header();
+get_header();
 ?>
