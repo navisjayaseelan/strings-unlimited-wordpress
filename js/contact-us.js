7ce8c4febca09d18a6c669ee073d01910ec8750d
Navis <michael.bearly@hotmail.com>
2019-02-18 17:34:35 +0530

    update

--- js/contact-us.js
+++ js/contact-us.js
@@ -1,33 +1,35 @@
-$("#contact-form").submit(function(event){
-	// cancels the form submission
-    event.preventDefault();
-    submitForm();
-});
+$(document).ready(function () {
+	$("#contact-form").submit(function(event){
+		// cancels the form submission
+	    event.preventDefault();
+	    submitForm();
+	});
 
-function submitForm(){
-    // Initiate Variables With Form Content
-    var title = $('#title').value; // required
-	var fname = $('#fname').value; // required
-	var lname = $('#lname').value; // required
-	var company = $('#company').value; // required
-	var industry = $('industry').value; // not required
-	var city = $('city').value; // required
-	var country = $('country').value;
-	var phone = $('phone').value;
-	var email = $('email').value;
-	var message = $('message').value;
+	function submitForm(){
+	    // Initiate Variables With Form Content
+	    var title = $('#title').value; // required
+		var fname = $('#fname').value; // required
+		var lname = $('#lname').value; // required
+		var company = $('#company').value; // required
+		var industry = $('#industry').value; // not required
+		var city = $('#city').value; // required
+		var country = $('#country').value;
+		var phone = $('#phone').value;
+		var email = $('#email').value;
+		var message = $('#message').value;
 
-    $.ajax({
-        type: "POST",
-        url: "contact-thank-you",
-        data: "title=" + title + "&fname=" + fname + "&lname=" + lname + "&company=" + company + "&industry=" + industry + "&city=" + city + "&country=" + country + "&phone=" + phone  + "&email=" + email  + "&message=" + message  ,
-        success : function(text){
-            if (text == "success"){
-                formSuccess();
-            }
-        }
-    });
-}
-function formSuccess(){
-    $( "#msgSubmit" ).removeClass( "hidden" );
+	    $.ajax({
+	        type: "POST",
+	        url: "contact-thank-you",
+	        data: "title=" + title + "&fname=" + fname + "&lname=" + lname + "&company=" + company + "&industry=" + industry + "&city=" + city + "&country=" + country + "&phone=" + phone  + "&email=" + email  + "&message=" + message  ,
+	        success : function(text){
+	            if (text == "success"){
+	                formSuccess();
+	            }
+	        }
+	    });
+	}
+	function formSuccess(){
+	    $( "#msgSubmit" ).removeClass( "hidden" );
+	}
 }
