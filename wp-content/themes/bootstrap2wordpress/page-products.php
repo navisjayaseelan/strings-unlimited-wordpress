638a5f6d1d9c02b10f8ff56f7f0ec46f55ea95d6
Navis <michael.bearly@hotmail.com>
2019-02-22 20:32:25 +0530

    update

--- wp-content/themes/bootstrap2wordpress/page-products.php
+++ wp-content/themes/bootstrap2wordpress/page-products.php
@@ -9,96 +9,6 @@
 
 
 
-            <div class="row">
-                <div class="col-lg-12 col-sm-12 section-tittle">
-                    <h1>
-                        Products
-                    </h1>
-                </div>
-                <div class="col-lg-4 col-sm-6 products-content">
-                    <!--
-                    <a href="/products/fashion/">
-                        <img src="<?php bloginfo( 'template_directory' ); ?>/assets/img/products-1.jpg">
-                        <h3>Fashion</h3>
-                    </a>
-                    -->
-                    <div class="card hovercard">
-                       <img src="<?php bloginfo( 'template_directory' ); ?>/assets/img/products-1.jpg" alt=""/>
-                       <div class="info">
-                       </div>
-                       <div class="bottom">
-                          <button class="btn btn-primary btn-lg"><a href="/products/fashion/">Fashion</a></button>
-                       </div>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-sm-12 products-content">
-                    <!--
-                    <a href="/products/corporate/">
-                        <img src="<?php bloginfo( 'template_directory' ); ?>/assets/img/products-2.jpg">
-                        <h3>Corporate</h3>
-                    </a>
-                    -->
-                    <div class="card hovercard">
-                       <img src="<?php bloginfo( 'template_directory' ); ?>/assets/img/products-2.jpg" alt=""/>
-                       <div class="info">
-                       </div>
-                       <div class="bottom">
-                          <button class="btn btn-primary btn-lg"><a href="/products/corporate/">Corporate</a></button>
-                       </div>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-sm-6 products-content">
-                    <!--
-                    <a href="/products/formals/">
-                        <img src="<?php bloginfo( 'template_directory' ); ?>/assets/img/products-3.jpg">
-                        <h3>Formals</h3>
-                    </a>
-                    -->
-                    <div class="card hovercard">
-                       <img src="<?php bloginfo( 'template_directory' ); ?>/assets/img/products-3.jpg" alt=""/>
-                       <div class="info">
-                       </div>
-                       <div class="bottom">
-                          <button class="btn btn-primary btn-lg"><a href="/products/formals/">Formals</a></button>
-                       </div>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-sm-6 products-content">
-                    <!--
-                    <a href="/products/uniforms/">
-                        <img src="<?php bloginfo( 'template_directory' ); ?>/assets/img/products-4.jpg">
-                        <h3>Uniforms</h3>
-                    </a>
-                    -->
-                    <div class="card hovercard">
-                       <img src="<?php bloginfo( 'template_directory' ); ?>/assets/img/products-4.jpg" alt=""/>
-                       <div class="info">
-                       </div>
-                       <div class="bottom">
-                          <button class="btn btn-primary btn-lg"> <a href="/products/uniforms/">Uniforms</a></button>
-                       </div>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-sm-6 products-content">
-                    <!--
-                    <a href="/products/leather/">
-                        <img src="<?php bloginfo( 'template_directory' ); ?>/assets/img/products-5.jpg">
-                        <h3>Leather</h3>
-                    </a>
-                    -->
-                    <div class="card hovercard">
-                       <img src="<?php bloginfo( 'template_directory' ); ?>/assets/img/products-5.jpg" alt=""/>
-                       <div class="info">
-                       </div>
-                       <div class="bottom">
-                          <button class="btn btn-primary btn-lg"><a href="/products/leather/">Leather</a></button>
-                       </div>
-                    </div>
-
-                </div>
-            </div>
-
-
 
 
             <div class="list">
@@ -109,7 +19,7 @@
         <div class="floater">
           
         </div>
-        <div class="strips-title">FASHION</div>
+        <div class="strips-title">Fashion</div>
         </header>  
         <figcaption>
           We can support our clients with a wide range of fabrics through our rapport with good Mills. Fabrics like Self Dobbies, Jacquards, Structural, Plains, Fabrics with special finishes like micro polish, micro sanding, Brushed, Peached, Fleeced, to name a few can be sourced by us on demand
@@ -123,10 +33,10 @@
         <div class="floater">
           
         </div>
-        
+         <div class="strips-title">Corporate</div>
         </header>  
         <figcaption>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+          We at Strings Unlimited believe that aCorporate WorkWear helps create a company it’s brand identity. Hence we combine our passion with our experience to offer custom-made Corporate Work Wear with top quality Fabrics and Trims to give value to your brand . Our experienced team of skilled professionals own your brand as much as you take pride of your brand.
         </figcaption>
       </figure>
     </article>
@@ -137,10 +47,10 @@
         <div class="floater">
           
         </div>
-         
+        <div class="strips-title">Formals</div>
         </header>  
         <figcaption>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+          We have a fine range of exquisite fabrics and trims especially for the dandy workaholics. Workwear could never be so much fun as it’s now. We offer detailed & handcrafted shirts, trousers, blazers & waistcoats for every formal occasion.
         </figcaption>
       </figure>
     </article>
@@ -151,10 +61,10 @@
         <div class="floater">
           
         </div>
-         
+          <div class="strips-title">Uniforms</div>
         </header>  
         <figcaption>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+          We also cater bulk uniforms for various verticals like educational institutions, railway, aviation, hotels, hospitals, factories, etc. Our creations are customized, from the design to production. We achieve innovative products, consistent with the identity of each client, with high standards of quality and a competitive cost for the market to which it is intended for.
         </figcaption>
       </figure>
     </article>
@@ -165,10 +75,10 @@
         <div class="floater">
           
         </div>
-          
+           <div class="strips-title">Leather</div>
         </header>  
         <figcaption>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+          With a State of Art facility Machinery, a plant capacity of 12,500 sqft and an integrated manufacturing facility we manufacture a wide range of quality Leather Products. With over 4 decades of experience in manufacturing finished Leather, our factory. All Leather products are meticulously crafted by skilled resources and each of our product reflects our workmanship& quality of work coupled with precision finish usinglatest technology. We support our clients with product development, sampling and bulk production of their brand.
         </figcaption>
       </figure>
     </article>
