e2cdf11b041bd2ef2c5bf14316a7aac2a45b8c1b
Navis <michael.bearly@hotmail.com>
2019-02-22 13:20:56 +0530

    update

---
+++ wp-content/themes/bootstrap2wordpress/page-production-support.php
@@ -0,0 +1,51 @@
+ <?php
+/* Template Name: Production Support - Strings Unlimited */
+	get_header();
+?>
+  <!--------- about-us-content start ----->
+    <!--------- about-us-content start ----->
+    <section class="production-support" id="top-fixed">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 col-sm-12 production-support-content wow slideInLeft animated">
+                    <h2>
+                        Production Support
+                    </h2>
+                    <ul>
+                        <li>
+                            Merchandising & Communications
+                        </li>
+                        <li>
+                            Fit /  Style Approvals
+                        </li>
+                        <li>
+                            Pre-Production Approvals
+                        </li>
+                        <li>
+                            Fabric Quality inspection
+                        </li>
+                        <li>
+                            Pre-Production Approvals
+                        </li>
+                        <li>
+                            Mid Production Checks
+                        </li>
+                        <li>
+                            In-line Inspections
+                        </li>
+                        <li>
+                            Mid Line Inspections
+                        </li>
+                        <li>
+                            Final Inspections
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--------- about-us-content end ------->
+<?php
+/* Template Name: Contact us for Strings Unlimited */
+	get_footer();
+?>
