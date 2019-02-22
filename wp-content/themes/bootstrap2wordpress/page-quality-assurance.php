093b8471462e4038bbf7de25997573f3ea9b57b6
Navis <michael.bearly@hotmail.com>
2019-02-22 13:24:15 +0530

    update

---
+++ wp-content/themes/bootstrap2wordpress/page-quality-assurance.php
@@ -0,0 +1,32 @@
+ <?php
+/* Template Name: Quality Assurance - Strings Unlimited */
+	get_header();
+?>
+<!--------- about-us-content start ----->
+    <section class="quality-assurance" id="top-fixed">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 col-sm-12 quality-assurance-content wow slideInRight animated">
+                    <h2>
+                        Quality Assurance
+                    </h2>
+                    <ul>
+                        <li>
+                            In addition to our internal quality checks, we also coordinate external Audits based on the Shipment Ports
+                        </li>
+                        <li>
+                            We have good working relationships with SEDEX , BSCI , WRAP, GLOBAL ORGANIC TEXTILE STANDARD and other renowned international Auditors
+                        </li>
+                        <li>
+                           We Organize and arrange professional and internationally accepted Quality Audits 
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--------- about-us-content end ------->
+<?php
+/* Template Name: Contact us for Strings Unlimited */
+	get_footer();
+?>
