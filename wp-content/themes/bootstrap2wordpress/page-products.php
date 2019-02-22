ae8ba6dd90179f838881f95091d422f5a12917aa
Navis <michael.bearly@hotmail.com>
2019-02-22 16:17:38 +0530

    update

--- wp-content/themes/bootstrap2wordpress/page-products.php
+++ wp-content/themes/bootstrap2wordpress/page-products.php
@@ -10,6 +10,8 @@
                     <h1>
                         Products
                     </h1>
+                    <br/>
+                    <br/>
                 </div>
                 <div class="col-lg-4 col-sm-6 products-content">
                     <a href="/products/fashion/">
