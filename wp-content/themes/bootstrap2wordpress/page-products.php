4ad92e29dd6d6c7b6ecebf09e606c6630ec0499e
Navis <michael.bearly@hotmail.com>
2019-02-22 20:52:59 +0530

    update

--- wp-content/themes/bootstrap2wordpress/page-products.php
+++ wp-content/themes/bootstrap2wordpress/page-products.php
@@ -6,99 +6,30 @@
     <section class="products" id="top-fixed">
         <div class="container">
 
-
-
-
-            <div class="row">
-                <div class="col-lg-12 col-sm-12 section-tittle">
+ <div class="row list">
+   <div class="col-lg-12 col-sm-12 section-tittle">
                     <h1>
                         Products
                     </h1>
                 </div>
-                <div class="col-lg-4 col-sm-6 products-content">
-                    <!--
-                    <a href="/products/fashion/">
-                        <img src="<?php bloginfo( 'template_directory' ); ?>/assets/img/products-1.jpg">
-                        <h3>Fashion</h3>
-                    </a>
-                    -->
-                    <div class="card hovercard">
-                       <img src="<?php bloginfo( 'template_directory' ); ?>/assets/img/products-1.jpg" alt=""/>
-                       <div class="info">
-                       </div>
-                       <div class="bottom">
-                          <button class="btn btn-primary btn-lg"><a href="/products/fashion/">Fashion</a></button>
-                       </div>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-sm-12 products-content">
-                    <!--
-                    <a href="/products/corporate/">
-                        <img src="<?php bloginfo( 'template_directory' ); ?>/assets/img/products-2.jpg">
-                        <h3>Corporate</h3>
-                    </a>
-                    -->
-                    <div class="card hovercard">
-                       <img src="<?php bloginfo( 'template_directory' ); ?>/assets/img/products-2.jpg" alt=""/>
-                       <div class="info">
-                       </div>
-                       <div class="bottom">
-                          <button class="btn btn-primary btn-lg"><a href="/products/corporate/">Corporate</a></button>
-                       </div>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-sm-6 products-content">
-                    <!--
-                    <a href="/products/formals/">
-                        <img src="<?php bloginfo( 'template_directory' ); ?>/assets/img/products-3.jpg">
-                        <h3>Formals</h3>
-                    </a>
-                    -->
-                    <div class="card hovercard">
-                       <img src="<?php bloginfo( 'template_directory' ); ?>/assets/img/products-3.jpg" alt=""/>
-                       <div class="info">
-                       </div>
-                       <div class="bottom">
-                          <button class="btn btn-primary btn-lg"><a href="/products/formals/">Formals</a></button>
-                       </div>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-sm-6 products-content">
-                    <!--
-                    <a href="/products/uniforms/">
-                        <img src="<?php bloginfo( 'template_directory' ); ?>/assets/img/products-4.jpg">
-                        <h3>Uniforms</h3>
-                    </a>
-                    -->
-                    <div class="card hovercard">
-                       <img src="<?php bloginfo( 'template_directory' ); ?>/assets/img/products-4.jpg" alt=""/>
-                       <div class="info">
-                       </div>
-                       <div class="bottom">
-                          <button class="btn btn-primary btn-lg"> <a href="/products/uniforms/">Uniforms</a></button>
-                       </div>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-sm-6 products-content">
-                    <!--
-                    <a href="/products/leather/">
-                        <img src="<?php bloginfo( 'template_directory' ); ?>/assets/img/products-5.jpg">
-                        <h3>Leather</h3>
-                    </a>
-                    -->
-                    <div class="card hovercard">
-                       <img src="<?php bloginfo( 'template_directory' ); ?>/assets/img/products-5.jpg" alt=""/>
-                       <div class="info">
-                       </div>
-                       <div class="bottom">
-                          <button class="btn btn-primary btn-lg"><a href="/products/leather/">Leather</a></button>
-                       </div>
-                    </div>
-
-                </div>
-            </div>
+   <div class="col-lg-4 col-sm-6 products-content list--item">
+      <figure>
+        <img src="//placehold.it/250x250" alt="">
+        <header>
+        <div class="floater">
+          
         </div>
-    </section>
+          <h2>Title</h2>
+        </header>  
+        <figcaption>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+        </figcaption>
+      </figure>
+    </div>
+  </div>
+</div>
+</section>
+
     <!--------- what-we-do  end ------->
 <?php
 /* Template Name: Contact us for Strings Unlimited */
