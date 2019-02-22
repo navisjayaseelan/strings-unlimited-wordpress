71c681c1e1e0c936ca23f6cc6b82912d4a46fa5e
Navis <michael.bearly@hotmail.com>
2019-02-22 15:41:23 +0530

    update

---
+++ wp-content/themes/bootstrap2wordpress/page-products.php
@@ -0,0 +1,28 @@
+ <?php
+/* Template Name: Products -  Strings Unlimited */
+	get_header();
+?>
+ <!--------- faq-section start ----->
+<section class="faq-section" id="top-fixed">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 col-sm-12 section-tittle">
+                <h1>
+                    Products
+                </h1>
+            </div>
+            <ul>
+            	<li><a href="/products/fashion/">Fashion</a></li>
+            	<li><a href="/products/corporate/">Corporate</a></li>
+            	<li><a href="/products/formals/">Formals</a></li>
+            	<li><a href="/products/uniforms/">Uniforms</a></li>
+            	<li><a href="/products/leather/">Leather</a></li>
+            </ul>
+        </div>
+    </div>
+</section>
+<!--------- faq-section end ------->
+<?php
+/* Template Name: Contact us for Strings Unlimited */
+	get_footer();
+?>
