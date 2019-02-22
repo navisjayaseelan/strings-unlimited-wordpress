0b6c84977cb13b7a4e006bd10ce23f1472ddcea9
Navis <michael.bearly@hotmail.com>
2019-02-22 13:09:30 +0530

    update

---
+++ wp-content/themes/bootstrap2wordpress/page-whatwedo.php
@@ -0,0 +1,54 @@
+<?php
+/* Template Name: What we do - Strings Unlimited */
+get_header();
+#Advanced custom field added by Navis
+$title = get_field('title');
+$content = get_field('content');
+?>
+<!--------- what-we-do start ----->
+<section class="what-we-do" id="top-fixed">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 col-sm-12 section-tittle">
+                <h1>
+                    What we do
+                </h1>
+            </div>
+            <div class="col-lg-4 col-sm-6 what-we-do-content">
+                <a href="rd-sourcing.html">
+                    <img src="<?php bloginfo( 'template_directory' ); ?>/assets/img/what-we-do-img1.jpg">
+                    <h3>R&D and Sourcing</h3>
+                </a>
+            </div>
+            <div class="col-lg-4 col-sm-12 what-we-do-content">
+                <a href="production-support.html">
+                    <img src="<?php bloginfo( 'template_directory' ); ?>/assets/img/what-we-do-img2.jpg">
+                    <h3>Production Support</h3>
+                </a>
+            </div>
+            <div class="col-lg-4 col-sm-6 what-we-do-content">
+                <a href="quality-assurance.html">
+                    <img src="<?php bloginfo( 'template_directory' ); ?>/assets/img/what-we-do-img3.jpg">
+                    <h3>Quality Assurance</h3>
+                </a>
+            </div>
+            <div class="col-lg-4 col-sm-6 what-we-do-content">
+                <a href="order-management.html">
+                    <img src="<?php bloginfo( 'template_directory' ); ?>/assets/img/what-we-do-img4.jpg">
+                    <h3>Order Management</h3>
+                </a>
+            </div>
+            <div class="col-lg-4 col-sm-6 what-we-do-content">
+                <a href="logisics.html">
+                    <img src="<?php bloginfo( 'template_directory' ); ?>/assets/img/what-we-do-img5.jpg">
+                    <h3>Logisic</h3>
+                </a>
+            </div>
+        </div>
+    </div>
+</section>
+<!--------- what-we-do  end ------->
+<?php
+/* Template Name: Contact us for Strings Unlimited */
+get_header();
+?>
