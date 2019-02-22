2d5b2d79a429fa9bc34b037b78c04826bd11f61d
Navis <michael.bearly@hotmail.com>
2019-02-22 16:56:04 +0530

    update

--- wp-content/themes/bootstrap2wordpress/page-whatwedo.php
+++ wp-content/themes/bootstrap2wordpress/page-whatwedo.php
@@ -12,16 +12,37 @@
                 </h1>
             </div>
             <div class="col-lg-4 col-sm-6 what-we-do-content">
+                <!--
                 <a href="/what-we-do/rd-and-sourcing/">
                     <img src="<?php bloginfo( 'template_directory' ); ?>/assets/img/what-we-do-img1.jpg">
                     <h3>R&D and Sourcing</h3>
                 </a>
+                -->
+                <div class="card hovercard">
+                   <img src="<?php bloginfo( 'template_directory' ); ?>/assets/img/what-we-do-img1.jpg" alt=""/>
+                   <div class="info">
+                   </div>
+                   <div class="bottom">
+                      <button class="btn btn-primary btn-lg">>R&D and Sourcing</button>
+                   </div>
+                </div>
             </div>
             <div class="col-lg-4 col-sm-12 what-we-do-content">
+                <!--
                 <a href="/what-we-do/production-support/">
                     <img src="<?php bloginfo( 'template_directory' ); ?>/assets/img/what-we-do-img2.jpg">
                     <h3>Production Support</h3>
                 </a>
+                -->
+                <div class="card hovercard">
+                   <img src="<?php bloginfo( 'template_directory' ); ?>/assets/img/what-we-do-img2.jpg" alt=""/>
+                   <div class="info">
+                   </div>
+                   <div class="bottom">
+                      <button class="btn btn-primary btn-lg">Production Support</button>
+                   </div>
+                </div>
+
             </div>
             <div class="col-lg-4 col-sm-6 what-we-do-content">
                 <a href="/what-we-do/quality-assurance/">
@@ -30,16 +51,37 @@
                 </a>
             </div>
             <div class="col-lg-4 col-sm-6 what-we-do-content">
+                <!--
                 <a href="/what-we-do/order-management/">
                     <img src="<?php bloginfo( 'template_directory' ); ?>/assets/img/what-we-do-img4.jpg">
                     <h3>Order Management</h3>
                 </a>
+                -->
+                <div class="card hovercard">
+                   <img src="<?php bloginfo( 'template_directory' ); ?>/assets/img/what-we-do-img4.jpg" alt=""/>
+                   <div class="info">
+                   </div>
+                   <div class="bottom">
+                      <button class="btn btn-primary btn-lg">Order Management</button>
+                   </div>
+                </div>
             </div>
             <div class="col-lg-4 col-sm-6 what-we-do-content">
+                <!--
                 <a href="/what-we-do/logistics/">
                     <img src="<?php bloginfo( 'template_directory' ); ?>/assets/img/what-we-do-img5.jpg">
                     <h3>Logistics</h3>
                 </a>
+                -->
+                <div class="card hovercard">
+                   <img src="<?php bloginfo( 'template_directory' ); ?>/assets/img/what-we-do-img5.jpg" alt=""/>
+                   <div class="info">
+                   </div>
+                   <div class="bottom">
+                      <button class="btn btn-primary btn-lg">Logistics</button>
+                   </div>
+                </div>
+
             </div>
         </div>
     </div>
