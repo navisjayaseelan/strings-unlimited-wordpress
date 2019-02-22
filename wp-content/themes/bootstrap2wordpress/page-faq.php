730c2f2331d235ed0194c77a9a6d3a5d7dbb394c
Navis <michael.bearly@hotmail.com>
2019-02-22 12:23:42 +0530

    faq template

---
+++ wp-content/themes/bootstrap2wordpress/page-faq.php
@@ -0,0 +1,78 @@
+ <?php
+/* Template Name: Faq for Strings Unlimited */
+	get_header();
+?>
+ <!--------- faq-section start ----->
+<section class="faq-section" id="top-fixed">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 col-sm-12 section-tittle">
+                <h1>
+                    Faq's
+                </h1>
+            </div>
+            <div class="col-lg-12 col-12 faq-section-content">
+                <div class="qa">
+                    <h2>
+                        What is the relation between Strings Unlimited Pvt Ltd, UK and Strings Factory Pvt Ltd, India?
+                    </h2>
+                    <p>
+                        Strings Factory Private Ltd., is the 100% owned subsidiary company of Strings Unlimited Pvt Ltd., UK. The Parent Company (Strings Unlimited Pvt Ltd) in UK has established this subsidiary company strategically in Chennai, India to cater our Products and Service to both clients based in India and Clients spread across the Globe. This gives us a competitive edge in the Apparel Industry as we have direct control over the production to deliver Products & Services with Quality that conforms to International Standard with competitive pricing that suits each individual client requirement.
+                    </p>
+                </div>
+
+                <div class="qa">
+                    <h2>
+                        I am interested in using your service how do I go about?
+                    </h2>
+                    <p>
+                        We are glad that you are interested to work with our team. Please contact us at +91 (0) 44 3256 3256 / +91 (0) 44 43831211 or alternatively send an email to our Sales team at sales@strings-unlimited.com.
+                    </p>
+                    <p>
+                        You can also go to our contact page and submit your enquiry from this website.
+                    </p>
+                    <p>
+                        As soon as we understand your requirement, we will either meet you in person with our samples or will send you our sample after you pay us a refundable admin fee (conditions apply) depending on the style you have shown interest.
+                    </p>
+                    <p>
+                        If you are satisfied with the quality of our samples and after you release the PO to us on your official letter head, we will make a Pre-Production sample for your review. If you are satisfied with the sample and after you make the advance payment or after receiving your sight LC document, we will start the production.
+                    </p>
+                </div>
+
+                <div class="qa">
+                    <h2>
+                        How does your company manage risk in business transactions?
+                    </h2>
+                    <p>
+                        We consciously promote risk free transactions to all our partners. This is part of our company values of being transparent with all our business partners. As much as we avoid taking risks in our business transactions we believe that our partners also should not in any way put in risk by working with us. Hence we have a policy of taking advance payment or arrange a sight LC (International Clients) to initiate the production process. Balance payments are either agreed to be paid on delivery of goods or maximum 7 business days from date of delivery of goods provided we are given a post-dated cheque by your company as a guarantee. Please refer to our T&C document which clearly underlines the conditions that are related to Balance payment collection.
+                    </p>
+                </div>
+                
+                <div class="qa">
+                    <h2>
+                        What are the acceptable mode of payments?
+                    </h2>
+                    <p>
+                        To make it convenient for all our partners we accept payments via any of the below methods:
+                    </p>
+                    <ul>
+                        <li>
+                           Cheque – (only within India) 
+                        </li>
+                        <li>
+                            Bank Transfer/Wire Transfer
+                        </li>
+                        <li>
+                            Letter Of Credit at Sight (Risk Free mode of payment for international payments)
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!--------- faq-section end ------->
+<?php
+/* Template Name: Contact us for Strings Unlimited */
+	get_footer();
+?>
