7b473b61becadd8296785e2768928c6af741654a
Navis <michael.bearly@hotmail.com>
2019-02-23 08:10:05 +0530

    update

--- wp-content/themes/bootstrap2wordpress/header.php
+++ wp-content/themes/bootstrap2wordpress/header.php
@@ -15,6 +15,7 @@
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,Chrome=1" />
 	<link rel="profile" href="https://gmpg.org/xfn/11">
 
 	 <link rel="stylesheet" type="text/css" href="<?php bloginfo( 'stylesheet_directory' ); ?>/assets/css/font-awesome.min.css">
@@ -30,7 +31,10 @@
     <!-- responsive -->
     <link rel="stylesheet" type="text/css" href="<?php bloginfo( 'stylesheet_directory' ); ?>/assets/css/responsive.css">
     
-	
+  	<!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
 
 	<?php wp_head(); ?>
 </head>
