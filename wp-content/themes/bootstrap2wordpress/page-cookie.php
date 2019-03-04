cbe40dad4fce02899241f51af2aa54db06be8861
Navis Michael Bearly J <michael.bearly@hotmail.com>
2019-03-04 21:19:00 +0530

    update

--- wp-content/themes/bootstrap2wordpress/page-cookie.php
+++ wp-content/themes/bootstrap2wordpress/page-cookie.php
@@ -15,43 +15,43 @@
 
  <div class="row list">
    <div class="col-lg-12 col-sm-12 section-tittle">
-                  <h1>Cookie Policy for strings-unlimited</h1>
+                  <h1 class="display-2">Cookie Policy for strings-unlimited</h1>
                 </div>
 <div class="col-lg-12 col-12 faq-section-content">
 
-  <p>This is the Cookie Policy for strings-unlimited, accessible from https://strings-unlimited.com/cookie-policy</p>
+  <p class="lead">This is the Cookie Policy for strings-unlimited, accessible from https://strings-unlimited.com/cookie-policy</p>
 
 
 
-<p><strong>What Are Cookies</strong></p>
+<p class="lead"><strong>What Are Cookies</strong></p>
 
 
 
-<p>As is common practice with almost all professional websites this site uses cookies, which are tiny files that are downloaded to your computer, to improve your experience. This page describes what information they gather, how we use it and why we sometimes need to store these cookies. We will also share how you can prevent these cookies from being stored however this may downgrade or 'break' certain elements of the sites functionality.</p>
+<p class="lead">As is common practice with almost all professional websites this site uses cookies, which are tiny files that are downloaded to your computer, to improve your experience. This page describes what information they gather, how we use it and why we sometimes need to store these cookies. We will also share how you can prevent these cookies from being stored however this may downgrade or 'break' certain elements of the sites functionality.</p>
 
 
 
-<p>For more general information on cookies see the Wikipedia article on HTTP Cookies.</p>
+<p class="lead">For more general information on cookies see the Wikipedia article on HTTP Cookies.</p>
 
 
 
-<p><strong>How We Use Cookies</strong></p>
+<p class="lead"><strong>How We Use Cookies</strong></p>
 
 
 
-<p>We use cookies for a variety of reasons detailed below. Unfortunately in most cases there are no industry standard options for disabling cookies without completely disabling the functionality and features they add to this site. It is recommended that you leave on all cookies if you are not sure whether you need them or not in case they are used to provide a service that you use.</p>
+<p class="lead">We use cookies for a variety of reasons detailed below. Unfortunately in most cases there are no industry standard options for disabling cookies without completely disabling the functionality and features they add to this site. It is recommended that you leave on all cookies if you are not sure whether you need them or not in case they are used to provide a service that you use.</p>
 
 
 
-<p><strong>Disabling Cookies</strong></p>
+<p class="lead"><strong>Disabling Cookies</strong></p>
 
 
 
-<p>You can prevent the setting of cookies by adjusting the settings on your browser (see your browser Help for how to do this). Be aware that disabling cookies will affect the functionality of this and many other websites that you visit. Disabling cookies will usually result in also disabling certain functionality and features of the this site. Therefore it is recommended that you do not disable cookies.</p>
+<p class="lead">You can prevent the setting of cookies by adjusting the settings on your browser (see your browser Help for how to do this). Be aware that disabling cookies will affect the functionality of this and many other websites that you visit. Disabling cookies will usually result in also disabling certain functionality and features of the this site. Therefore it is recommended that you do not disable cookies.</p>
 
 
 
-<p><strong>The Cookies We Set</strong></p>
+<p class="lead"><strong>The Cookies We Set</strong></p>
 
 
 
