03bf7eb6b71b8fb0bb897aa97983f1562f2216b5
Navis Michael Bearly J <michael.bearly@hotmail.com>
2019-03-04 21:14:50 +0530

    update

--- wp-content/themes/bootstrap2wordpress/page-cookie.php
+++ wp-content/themes/bootstrap2wordpress/page-cookie.php
@@ -11,47 +11,153 @@
                   <h1>Cookie Policy for strings-unlimited</h1>
                 </div>
 <div class="col-lg-12 col-12 faq-section-content">
-              <p>ThisistheCookiePolicyforstrings-unlimited,accessiblefromhttps://strings-unlimited.com/</p>
-              <p><strong>WhatAreCookies</strong></p>
-              <p>Asiscommonpracticewithalmostallprofessionalwebsitesthissiteusescookies,whicharetinyfilesthataredownloadedtoyourcomputer,toimproveyourexperience.Thispagedescribeswhatinformationtheygather,howweuseitandwhywesometimesneedtostorethesecookies.Wewillalsosharehowyoucanpreventthesecookiesfrombeingstoredhoweverthismaydowngradeor'break'certainelementsofthesitesfunctionality.</p>
-              <p>FormoregeneralinformationoncookiesseetheWikipediaarticleonHTTPCookies.</p>
-              <p><strong>HowWeUseCookies</strong></p>
-              <p>Weusecookiesforavarietyofreasonsdetailedbelow.Unfortunatelyinmostcasestherearenoindustrystandardoptionsfordisablingcookieswithoutcompletelydisablingthefunctionalityandfeaturestheyaddtothissite.Itisrecommendedthatyouleaveonallcookiesifyouarenotsurewhetheryouneedthemornotincasetheyareusedtoprovideaservicethatyouuse.</p>
-              <p><strong>DisablingCookies</strong></p>
-              <p>Youcanpreventthesettingofcookiesbyadjustingthesettingsonyourbrowser(seeyourbrowserHelpforhowtodothis).Beawarethatdisablingcookieswillaffectthefunctionalityofthisandmanyotherwebsitesthatyouvisit.Disablingcookieswillusuallyresultinalsodisablingcertainfunctionalityandfeaturesofthethissite.Thereforeitisrecommendedthatyoudonotdisablecookies.</p>
-              <p><strong>TheCookiesWeSet</strong></p>
-              <ul>
-                 <li>
-                    <p>Formsrelatedcookies</p>
-                    <p>Whenyousubmitdatatothroughaformsuchasthosefoundoncontactpagesorcommentformscookiesmaybesettorememberyouruserdetailsforfuturecorrespondence.</p>
-                 </li>
-                 <li>
-                    <p>Sitepreferencescookies</p>
-                    <p>Inordertoprovideyouwithagreatexperienceonthissiteweprovidethefunctionalitytosetyourpreferencesforhowthissiterunswhenyouuseit.Inordertorememberyourpreferencesweneedtosetcookiessothatthisinformationcanbecalledwheneveryouinteractwithapageisaffectedbyyourpreferences.</p>
-                 </li>
-              </ul>
-              <p><strong>ThirdPartyCookies</strong></p>
-              <p>Insomespecialcaseswealsousecookiesprovidedbytrustedthirdparties.Thefollowingsectiondetailswhichthirdpartycookiesyoumightencounterthroughthissite.</p>
-              <ul>
-                 <li>
-                    <p>ThissiteusesGoogleAnalyticswhichisoneofthemostwidespreadandtrustedanalyticssolutiononthewebforhelpingustounderstandhowyouusethesiteandwaysthatwecanimproveyourexperience.Thesecookiesmaytrackthingssuchashowlongyouspendonthesiteandthepagesthatyouvisitsowecancontinuetoproduceengagingcontent.</p>
-                    <p>FormoreinformationonGoogleAnalyticscookies,seetheofficialGoogleAnalyticspage.</p>
-                 </li>
-                 <li>
-                    <p>Aswesellproductsit'simportantforustounderstandstatisticsabouthowmanyofthevisitorstooursiteactuallymakeapurchaseandassuchthisisthekindofdatathatthesecookieswilltrack.Thisisimportanttoyouasitmeansthatwecanaccuratelymakebusinesspredictionsthatallowustomonitorouradvertisingandproductcoststoensurethebestpossibleprice.</p>
-                 </li>
-                 <li>
-                    <p>Wealsousesocialmediabuttonsand/orpluginsonthissitethatallowyoutoconnectwithyoursocialnetworkinvariousways.Forthesetoworkthefollowingsocialmediasitesincluding;{Listthesocialnetworkswhosefeaturesyouhaveintegratedwithyoursite?:12},willsetcookiesthroughoursitewhichmaybeusedtoenhanceyourprofileontheirsiteorcontributetothedatatheyholdforvariouspurposesoutlinedintheirrespectiveprivacypolicies.</p>
-                 </li>
-              </ul>
-              <p><strong>MoreInformation</strong></p>
-              <p>Hopefullythathasclarifiedthingsforyouandaswaspreviouslymentionedifthereissomethingthatyouaren'tsurewhetheryouneedornotit'susuallysafertoleavecookiesenabledincaseitdoesinteractwithoneofthefeaturesyouuseonoursite.ThisCookiesPolicywascreatedwiththehelpoftheGeneratorof<ahref="https://cookiepolicygenerator.com">GDPRCookiesPolicyTemplate</a>andthe<ahref="https://cookiespolicytemplate.com/">GDPRCookiesPolicyTemplate</a>.</p>
-              <p>Howeverifyouarestilllookingformoreinformationthenyoucancontactusthroughoneofourpreferredcontactmethods:</p>
-              <ul>
-                 <li>Email:</li>
-                 <li>Byvisitingthislink:</li>
-                 <li>Phone:</li>
-              </ul>
+  <p>This is the Cookie Policy for strings-unlimited, accessible from https://strings-unlimited.com/cookie-policy</p>
+
+
+
+<p><strong>What Are Cookies</strong></p>
+
+
+
+<p>As is common practice with almost all professional websites this site uses cookies, which are tiny files that are downloaded to your computer, to improve your experience. This page describes what information they gather, how we use it and why we sometimes need to store these cookies. We will also share how you can prevent these cookies from being stored however this may downgrade or 'break' certain elements of the sites functionality.</p>
+
+
+
+<p>For more general information on cookies see the Wikipedia article on HTTP Cookies.</p>
+
+
+
+<p><strong>How We Use Cookies</strong></p>
+
+
+
+<p>We use cookies for a variety of reasons detailed below. Unfortunately in most cases there are no industry standard options for disabling cookies without completely disabling the functionality and features they add to this site. It is recommended that you leave on all cookies if you are not sure whether you need them or not in case they are used to provide a service that you use.</p>
+
+
+
+<p><strong>Disabling Cookies</strong></p>
+
+
+
+<p>You can prevent the setting of cookies by adjusting the settings on your browser (see your browser Help for how to do this). Be aware that disabling cookies will affect the functionality of this and many other websites that you visit. Disabling cookies will usually result in also disabling certain functionality and features of the this site. Therefore it is recommended that you do not disable cookies.</p>
+
+
+
+<p><strong>The Cookies We Set</strong></p>
+
+
+
+<ul>
+
+
+
+
+
+
+
+
+
+
+
+
+
+<li>
+
+<p>Forms related cookies</p>
+
+<p>When you submit data to through a form such as those found on contact pages or comment forms cookies may be set to remember your user details for future correspondence.</p>
+
+</li>
+
+
+
+<li>
+
+<p>Site preferences cookies</p>
+
+<p>In order to provide you with a great experience on this site we provide the functionality to set your preferences for how this site runs when you use it. In order to remember your preferences we need to set cookies so that this information can be called whenever you interact with a page is affected by your preferences.</p>
+
+</li>
+
+
+
+</ul>
+
+
+
+<p><strong>Third Party Cookies</strong></p>
+
+
+
+<p>In some special cases we also use cookies provided by trusted third parties. The following section details which third party cookies you might encounter through this site.</p>
+
+
+
+<ul>
+
+
+
+<li>
+
+<p>This site uses Google Analytics which is one of the most widespread and trusted analytics solution on the web for helping us to understand how you use the site and ways that we can improve your experience. These cookies may track things such as how long you spend on the site and the pages that you visit so we can continue to produce engaging content.</p>
+
+<p>For more information on Google Analytics cookies, see the official Google Analytics page.</p>
+
+</li>
+
+
+
+
+
+
+
+<li>
+
+<p>As we sell products it's important for us to understand statistics about how many of the visitors to our site actually make a purchase and as such this is the kind of data that these cookies will track. This is important to you as it means that we can accurately make business predictions that allow us to monitor our advertising and product costs to ensure the best possible price.</p>
+
+</li>
+
+
+
+
+
+
+
+
+
+
+
+<li>
+
+<p>We also use social media buttons and/or plugins on this site that allow you to connect with your social network in various ways. For these to work the following social media sites including; {List the social networks whose features you have integrated with your site?:12}, will set cookies through our site which may be used to enhance your profile on their site or contribute to the data they hold for various purposes outlined in their respective privacy policies.</p>
+
+</li>
+
+
+
+</ul>
+
+
+
+<p><strong>More Information</strong></p>
+
+
+
+<p>Hopefully that has clarified things for you and as was previously mentioned if there is something that you aren't sure whether you need or not it's usually safer to leave cookies enabled in case it does interact with one of the features you use on our site. This Cookies Policy was created with the help of the Generator of <a href="https://cookiepolicygenerator.com">GDPR Cookies Policy Template</a> and the <a href="https://cookiespolicytemplate.com/">GDPR Cookies Policy Template</a>.</p>
+
+
+
+<p>However if you are still looking for more information then you can contact us through one of our preferred contact methods:</p>
+
+
+
+<ul>
+
+<li>Email: </li>
+
+<li>By visiting this link: </li>
+
+<li>Phone: </li></ul>
 
     </div>
   </div>
