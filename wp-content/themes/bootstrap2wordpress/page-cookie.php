00b7ab26acdea48ff735104b4f1c3d436ba1ed84
Navis Michael Bearly J <michael.bearly@hotmail.com>
2019-03-04 21:01:54 +0530

    update

---
+++ wp-content/themes/bootstrap2wordpress/page-cookie.php
@@ -0,0 +1,65 @@
+ <?php
+/* Template Name: Products -  Strings Unlimited */
+	get_header();
+?>
+<!--------- what-we-do start ----->
+    <section class="products" id="top-fixed">
+        <div class="container">
+
+ <div class="row list">
+   <div class="col-lg-12 col-sm-12 section-tittle">
+                  <h1>Cookie Policy for strings-unlimited</h1>
+                </div>
+<div class="col-lg-12 col-12 faq-section-content">
+              <p>ThisistheCookiePolicyforstrings-unlimited,accessiblefromhttps://strings-unlimited.com/</p>
+              <p><strong>WhatAreCookies</strong></p>
+              <p>Asiscommonpracticewithalmostallprofessionalwebsitesthissiteusescookies,whicharetinyfilesthataredownloadedtoyourcomputer,toimproveyourexperience.Thispagedescribeswhatinformationtheygather,howweuseitandwhywesometimesneedtostorethesecookies.Wewillalsosharehowyoucanpreventthesecookiesfrombeingstoredhoweverthismaydowngradeor'break'certainelementsofthesitesfunctionality.</p>
+              <p>FormoregeneralinformationoncookiesseetheWikipediaarticleonHTTPCookies.</p>
+              <p><strong>HowWeUseCookies</strong></p>
+              <p>Weusecookiesforavarietyofreasonsdetailedbelow.Unfortunatelyinmostcasestherearenoindustrystandardoptionsfordisablingcookieswithoutcompletelydisablingthefunctionalityandfeaturestheyaddtothissite.Itisrecommendedthatyouleaveonallcookiesifyouarenotsurewhetheryouneedthemornotincasetheyareusedtoprovideaservicethatyouuse.</p>
+              <p><strong>DisablingCookies</strong></p>
+              <p>Youcanpreventthesettingofcookiesbyadjustingthesettingsonyourbrowser(seeyourbrowserHelpforhowtodothis).Beawarethatdisablingcookieswillaffectthefunctionalityofthisandmanyotherwebsitesthatyouvisit.Disablingcookieswillusuallyresultinalsodisablingcertainfunctionalityandfeaturesofthethissite.Thereforeitisrecommendedthatyoudonotdisablecookies.</p>
+              <p><strong>TheCookiesWeSet</strong></p>
+              <ul>
+                 <li>
+                    <p>Formsrelatedcookies</p>
+                    <p>Whenyousubmitdatatothroughaformsuchasthosefoundoncontactpagesorcommentformscookiesmaybesettorememberyouruserdetailsforfuturecorrespondence.</p>
+                 </li>
+                 <li>
+                    <p>Sitepreferencescookies</p>
+                    <p>Inordertoprovideyouwithagreatexperienceonthissiteweprovidethefunctionalitytosetyourpreferencesforhowthissiterunswhenyouuseit.Inordertorememberyourpreferencesweneedtosetcookiessothatthisinformationcanbecalledwheneveryouinteractwithapageisaffectedbyyourpreferences.</p>
+                 </li>
+              </ul>
+              <p><strong>ThirdPartyCookies</strong></p>
+              <p>Insomespecialcaseswealsousecookiesprovidedbytrustedthirdparties.Thefollowingsectiondetailswhichthirdpartycookiesyoumightencounterthroughthissite.</p>
+              <ul>
+                 <li>
+                    <p>ThissiteusesGoogleAnalyticswhichisoneofthemostwidespreadandtrustedanalyticssolutiononthewebforhelpingustounderstandhowyouusethesiteandwaysthatwecanimproveyourexperience.Thesecookiesmaytrackthingssuchashowlongyouspendonthesiteandthepagesthatyouvisitsowecancontinuetoproduceengagingcontent.</p>
+                    <p>FormoreinformationonGoogleAnalyticscookies,seetheofficialGoogleAnalyticspage.</p>
+                 </li>
+                 <li>
+                    <p>Aswesellproductsit'simportantforustounderstandstatisticsabouthowmanyofthevisitorstooursiteactuallymakeapurchaseandassuchthisisthekindofdatathatthesecookieswilltrack.Thisisimportanttoyouasitmeansthatwecanaccuratelymakebusinesspredictionsthatallowustomonitorouradvertisingandproductcoststoensurethebestpossibleprice.</p>
+                 </li>
+                 <li>
+                    <p>Wealsousesocialmediabuttonsand/orpluginsonthissitethatallowyoutoconnectwithyoursocialnetworkinvariousways.Forthesetoworkthefollowingsocialmediasitesincluding;{Listthesocialnetworkswhosefeaturesyouhaveintegratedwithyoursite?:12},willsetcookiesthroughoursitewhichmaybeusedtoenhanceyourprofileontheirsiteorcontributetothedatatheyholdforvariouspurposesoutlinedintheirrespectiveprivacypolicies.</p>
+                 </li>
+              </ul>
+              <p><strong>MoreInformation</strong></p>
+              <p>Hopefullythathasclarifiedthingsforyouandaswaspreviouslymentionedifthereissomethingthatyouaren'tsurewhetheryouneedornotit'susuallysafertoleavecookiesenabledincaseitdoesinteractwithoneofthefeaturesyouuseonoursite.ThisCookiesPolicywascreatedwiththehelpoftheGeneratorof<ahref="https://cookiepolicygenerator.com">GDPRCookiesPolicyTemplate</a>andthe<ahref="https://cookiespolicytemplate.com/">GDPRCookiesPolicyTemplate</a>.</p>
+              <p>Howeverifyouarestilllookingformoreinformationthenyoucancontactusthroughoneofourpreferredcontactmethods:</p>
+              <ul>
+                 <li>Email:</li>
+                 <li>Byvisitingthislink:</li>
+                 <li>Phone:</li>
+              </ul>
+
+    </div>
+  </div>
+</div>
+</section>
+
+    <!--------- what-we-do  end ------->
+<?php
+/* Template Name: Contact us for Strings Unlimited */
+	get_footer();
+?>
