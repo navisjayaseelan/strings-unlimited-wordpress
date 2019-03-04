97222ca89e344c812a1795c5ae95390c193abec8
Navis Michael Bearly J <michael.bearly@hotmail.com>
2019-03-04 21:21:42 +0530

    update

--- wp-content/themes/bootstrap2wordpress/page-cookie.php
+++ wp-content/themes/bootstrap2wordpress/page-cookie.php
@@ -7,6 +7,7 @@
 .faq-section-content p {
   text-align: justify;
   text-justify: inter-word;
+  padding-bottom: 15px;
 }
 </style>
 <!--------- what-we-do start ----->
@@ -18,7 +19,7 @@
                   <h1 class="display-2">Cookie Policy for strings-unlimited</h1>
                 </div>
 <div class="col-lg-12 col-12 faq-section-content">
-
+<br/>
   <p class="lead">This is the Cookie Policy for strings-unlimited, accessible from https://strings-unlimited.com/cookie-policy</p>
 
 
@@ -55,7 +56,7 @@
 
 
 
-<ul>
+<ul  class="lead">
 
 
 
@@ -71,9 +72,9 @@
 
 <li>
 
-<p>Forms related cookies</p>
+<p  class="lead">Forms related cookies</p>
 
-<p>When you submit data to through a form such as those found on contact pages or comment forms cookies may be set to remember your user details for future correspondence.</p>
+<p  class="lead">When you submit data to through a form such as those found on contact pages or comment forms cookies may be set to remember your user details for future correspondence.</p>
 
 </li>
 
@@ -81,63 +82,9 @@
 
 <li>
 
-<p>Site preferences cookies</p>
+<p  class="lead">Site preferences cookies</p>
 
-<p>In order to provide you with a great experience on this site we provide the functionality to set your preferences for how this site runs when you use it. In order to remember your preferences we need to set cookies so that this information can be called whenever you interact with a page is affected by your preferences.</p>
-
-</li>
-
-
-
-</ul>
-
-
-
-<p><strong>Third Party Cookies</strong></p>
-
-
-
-<p>In some special cases we also use cookies provided by trusted third parties. The following section details which third party cookies you might encounter through this site.</p>
-
-
-
-<ul>
-
-
-
-<li>
-
-<p>This site uses Google Analytics which is one of the most widespread and trusted analytics solution on the web for helping us to understand how you use the site and ways that we can improve your experience. These cookies may track things such as how long you spend on the site and the pages that you visit so we can continue to produce engaging content.</p>
-
-<p>For more information on Google Analytics cookies, see the official Google Analytics page.</p>
-
-</li>
-
-
-
-
-
-
-
-<li>
-
-<p>As we sell products it's important for us to understand statistics about how many of the visitors to our site actually make a purchase and as such this is the kind of data that these cookies will track. This is important to you as it means that we can accurately make business predictions that allow us to monitor our advertising and product costs to ensure the best possible price.</p>
-
-</li>
-
-
-
-
-
-
-
-
-
-
-
-<li>
-
-<p>We also use social media buttons and/or plugins on this site that allow you to connect with your social network in various ways. For these to work the following social media sites including; {List the social networks whose features you have integrated with your site?:12}, will set cookies through our site which may be used to enhance your profile on their site or contribute to the data they hold for various purposes outlined in their respective privacy policies.</p>
+<p  class="lead">In order to provide you with a great experience on this site we provide the functionality to set your preferences for how this site runs when you use it. In order to remember your preferences we need to set cookies so that this information can be called whenever you interact with a page is affected by your preferences.</p>
 
 </li>
 
@@ -147,19 +94,73 @@
 
 
 
-<p><strong>More Information</strong></p>
+<p  class="lead"><strong>Third Party Cookies</strong></p>
 
 
 
-<p>Hopefully that has clarified things for you and as was previously mentioned if there is something that you aren't sure whether you need or not it's usually safer to leave cookies enabled in case it does interact with one of the features you use on our site. This Cookies Policy was created with the help of the Generator of <a href="https://cookiepolicygenerator.com">GDPR Cookies Policy Template</a> and the <a href="https://cookiespolicytemplate.com/">GDPR Cookies Policy Template</a>.</p>
+<p  class="lead">In some special cases we also use cookies provided by trusted third parties. The following section details which third party cookies you might encounter through this site.</p>
 
 
 
-<p>However if you are still looking for more information then you can contact us through one of our preferred contact methods:</p>
+<ul  class="lead">
 
 
 
-<ul>
+<li>
+
+<p  class="lead">This site uses Google Analytics which is one of the most widespread and trusted analytics solution on the web for helping us to understand how you use the site and ways that we can improve your experience. These cookies may track things such as how long you spend on the site and the pages that you visit so we can continue to produce engaging content.</p>
+
+<p  class="lead">For more information on Google Analytics cookies, see the official Google Analytics page.</p>
+
+</li>
+
+
+
+
+
+
+
+<li>
+
+<p  class="lead">As we sell products it's important for us to understand statistics about how many of the visitors to our site actually make a purchase and as such this is the kind of data that these cookies will track. This is important to you as it means that we can accurately make business predictions that allow us to monitor our advertising and product costs to ensure the best possible price.</p>
+
+</li>
+
+
+
+
+
+
+
+
+
+
+
+<li>
+
+<p  class="lead">We also use social media buttons and/or plugins on this site that allow you to connect with your social network in various ways. For these to work the following social media sites including; {List the social networks whose features you have integrated with your site?:12}, will set cookies through our site which may be used to enhance your profile on their site or contribute to the data they hold for various purposes outlined in their respective privacy policies.</p>
+
+</li>
+
+
+
+</ul>
+
+
+
+<p  class="lead"><strong>More Information</strong></p>
+
+
+
+<p  class="lead">Hopefully that has clarified things for you and as was previously mentioned if there is something that you aren't sure whether you need or not it's usually safer to leave cookies enabled in case it does interact with one of the features you use on our site. This Cookies Policy was created with the help of the Generator of <a href="https://cookiepolicygenerator.com">GDPR Cookies Policy Template</a> and the <a href="https://cookiespolicytemplate.com/">GDPR Cookies Policy Template</a>.</p>
+
+
+
+<p  class="lead">However if you are still looking for more information then you can contact us through one of our preferred contact methods:</p>
+
+
+
+<ul  class="lead">
 
 <li>Email: </li>
 
