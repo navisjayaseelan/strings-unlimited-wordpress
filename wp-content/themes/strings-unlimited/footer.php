9a845ec2edaf91f05549e9799f0d12bd764ab3e2
Navis Michael Bearly J <michael.bearly@hotmail.com>
2019-03-07 17:16:35 +0530

    update

--- wp-content/themes/strings-unlimited/footer.php
+++ wp-content/themes/strings-unlimited/footer.php
@@ -136,7 +136,7 @@
                     
                     <!--Copyright-->
                     <div class="copyright">&copy; Copyrights © 2019 - Strings Unlimited. All rights reserved</div>
-                    
+                    <div class="copyright pull-right">designed and developed by Freesie Infotech</div>
                 </div>
             </div>
         </div>
