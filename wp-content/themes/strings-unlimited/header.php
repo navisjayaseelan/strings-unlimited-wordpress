245fbf5ef2310f8e1882c0df86e06a9b6d536735
Navis Michael Bearly J <michael.bearly@hotmail.com>
2019-03-07 20:25:02 +0530

    update

--- wp-content/themes/strings-unlimited/header.php
+++ wp-content/themes/strings-unlimited/header.php
@@ -27,7 +27,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
 	<link href="<?php bloginfo('stylesheet_directory'); ?>/assets/css/responsive.css" rel="stylesheet">
 	
-	<?php wp_head(); ?>
+	<?php //wp_head(); ?>
 	<!--[if lt IE 9]><script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
 	<!--[if lt IE 9]><script src="js/respond.js"></script><![endif]-->
 </head>
