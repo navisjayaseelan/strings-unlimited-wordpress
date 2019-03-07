1bc47be94f26fc2c9a07f688f66a96c5bd92f059
Navis Michael Bearly J <michael.bearly@hotmail.com>
2019-03-07 17:57:10 +0530

    update

--- wp-content/themes/strings-unlimited/page-aboutus.php
+++ wp-content/themes/strings-unlimited/page-aboutus.php
@@ -3,22 +3,144 @@
 	get_header();
 ?>
  <!--------- about-us-content start ----->
-    <section class="about-us-content" id="top-fixed">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12 col-12 about-us-content-sub">
-                    <p>
-                         <?php echo get_field('about_us_content'); ?>
-                    </p>
-                    <div class="text-center">
-                        <a href="why-us/">
-                            <button>Why Us</button>
-                        </a>
+    <!--Page Title-->
+    <section class="page-title">
+        <div class="auto-container text-center">
+        
+            <h4 class="small-text">SINCE 2004</h4>
+            <h2 class="page-name">ABOUT US</h2>
+            
+            <!--Bread Crumb-->
+            <ul class="bread-crumb clearfix">
+                <li><a href="#">Home</a></li>
+                <li class="active">About Us</li>
+            </ul>
+            
+        </div>
+    </section>
+    
+    
+    <!--Tabs Section-->
+    <section class="tabs-section default-section with-padding">
+        <div class="auto-container">
+            <!--Section Title-->
+            <div class="sec-title text-center">
+                <h3>We’re Incredible!</h3>
+                <h2>Why Choose Us?</h2>
+                <div class="slices"><span class="slice"></span><span class="slice"></span><span class="slice"></span></div>
+            </div>
+            
+            <div class="outer-container">
+                <!--Tabs Box-->
+                <div class="tabs-box">
+                    
+                    <!--Tab Buttons-->
+                    <div class="tab-buttons clearfix">
+                        <a href="#tab-one" class="tab-btn active-btn">We Care Our Customers</a>
+                        <a href="#tab-two" class="tab-btn">Best Supports Ever</a>
+                        <a href="#tab-three" class="tab-btn">Very Quick Responses</a>
+                        <a href="#tab-four" class="tab-btn">Chat With Us Anytime</a>                                                                                                             
+                    </div>
+                    
+                    <!--Tabs Content-->
+                    <div class="tab-content">
+                        
+                        <!--Tab / Active Tab-->
+                        <div class="tab active-tab" id="tab-one">
+                            <div class="row clearfix">
+                                <!--Content Column-->
+                                <div class="content-column col-lg-7 col-sm-7 col-xs-12">
+                                    <h3>Strings-Unlimited is A Real Beautiful Theme</h3>
+                                    <div class="upper-content">
+                                        <div class="text">
+                                            <p>Classifieds are usually the first place you think of when you are getting ready to make a purchase. Whether you want to purchase a vehicle, bed, or pet, the classified section of your local newspaper can be one of the best resources available. With the emergence of the web however, many sites are being developed to host free classifieds.</p>
+                                            <p>More and more people are using the web daily; making these classified sites some of the most popular sites around. Free classified ad sites are great for other things as well however.</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                
+                                <!--Image Column-->
+                                <div class="image-column col-lg-5 col-sm-5 col-xs-12">
+                                    <figure class="image-box"><img src="images/resource/featured-image-1.jpg" alt=""><div class="over-layer"><span class="left-layer"></span><span class="right-layer"></span></div></figure>
+                                </div>
+                                
+                            </div>
+                        </div>
+                        
+                        <!--Tab-->
+                        <div class="tab" id="tab-two">
+                            <div class="row clearfix">
+                                <!--Content Column-->
+                                <div class="content-column col-lg-7 col-sm-7 col-xs-12">
+                                    <h3>Experience the Best Support.</h3>
+                                    <div class="upper-content">
+                                        <div class="text">
+                                            <p>Classifieds are usually the first place you think of when you are getting ready to make a purchase. Whether you want to purchase a vehicle, bed, or pet, the classified section of your local newspaper can be one of the best resources available. With the emergence of the web however, many sites are being developed to host free classifieds.</p>
+                                            <p>More and more people are using the web daily; making these classified sites some of the most popular sites around. Free classified ad sites are great for other things as well however.</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                
+                                <!--Image Column-->
+                                <div class="image-column col-lg-5 col-sm-5 col-xs-12">
+                                    <figure class="image-box"><img src="images/resource/featured-image-1.jpg" alt=""><div class="over-layer"><span class="left-layer"></span><span class="right-layer"></span></div></figure>
+                                </div>
+                                
+                            </div>
+                        </div>
+                        
+                        <!--Tab-->
+                        <div class="tab" id="tab-three">
+                            <div class="row clearfix">
+                                <!--Content Column-->
+                                <div class="content-column col-lg-7 col-sm-7 col-xs-12">
+                                    <h3>Advertising Laying Your Cards On The Table.</h3>
+                                    <div class="upper-content">
+                                        <div class="text">
+                                            <p>Classifieds are usually the first place you think of when you are getting ready to make a purchase. Whether you want to purchase a vehicle, bed, or pet, the classified section of your local newspaper can be one of the best resources available. With the emergence of the web however, many sites are being developed to host free classifieds.</p>
+                                            <p>More and more people are using the web daily; making these classified sites some of the most popular sites around. Free classified ad sites are great for other things as well however.</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                
+                                <!--Image Column-->
+                                <div class="image-column col-lg-5 col-sm-5 col-xs-12">
+                                    <figure class="image-box"><img src="images/resource/featured-image-1.jpg" alt=""><div class="over-layer"><span class="left-layer"></span><span class="right-layer"></span></div></figure>
+                                </div>
+                                
+                            </div>
+                        </div>
+                        
+                        <!--Tab-->
+                        <div class="tab" id="tab-four">
+                            <div class="row clearfix">
+                                <!--Content Column-->
+                                <div class="content-column col-lg-7 col-sm-7 col-xs-12">
+                                    <h3>24/7 Support</h3>
+                                    <div class="upper-content">
+                                        <div class="text">
+                                            <p>Classifieds are usually the first place you think of when you are getting ready to make a purchase. Whether you want to purchase a vehicle, bed, or pet, the classified section of your local newspaper can be one of the best resources available. With the emergence of the web however, many sites are being developed to host free classifieds.</p>
+                                            <p>More and more people are using the web daily; making these classified sites some of the most popular sites around. Free classified ad sites are great for other things as well however.</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                
+                                <!--Image Column-->
+                                <div class="image-column col-lg-5 col-sm-5 col-xs-12">
+                                    <figure class="image-box"><img src="images/resource/featured-image-1.jpg" alt=""><div class="over-layer"><span class="left-layer"></span><span class="right-layer"></span></div></figure>
+                                </div>
+                                
+                            </div>
+                        </div>
+                        
                     </div>
                 </div>
             </div>
+            
         </div>
     </section>
+    
+    
 <!--------- about-us-content end ------->
 <?php
 /* Template Name: Contact us for Strings Unlimited */
