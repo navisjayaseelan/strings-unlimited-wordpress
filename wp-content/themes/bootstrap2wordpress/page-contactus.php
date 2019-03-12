eb6b7da39631a0d416974d0cac3871ee15711989
Navis Michael Bearly J <michael.bearly@hotmail.com>
2019-03-12 19:42:16 +0530

    update

--- wp-content/themes/bootstrap2wordpress/page-contactus.php
+++ wp-content/themes/bootstrap2wordpress/page-contactus.php
@@ -102,7 +102,7 @@
 <script type="text/javascript">
   $('document').ready(function() {
     document.addEventListener( 'wpcf7submit', function( event ) {
-      if ( '378' == event.detail.contactFormId ) {
+      if ( '378' == event.detail.contactFormId && $('.wpcf7-response-output').toggle ) {
         jQuery.noConflict();
         $('#dummyModal').modal('show');
         $('.wpcf7-response-output').hide();
